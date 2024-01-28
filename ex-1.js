6ac1f4b3e08c8887c69ae9d177a538560e1c7b5e
Pacharadanai <pacharadanaipinake@gmail.com>
2024-01-28 22:08:05 +0700

    feat: add completed problem solving

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,13 @@
-function pivotIndex(nums) {}
+function pivotIndex(nums) {
+    const pivotNum = nums.find((_, index) => {
+        const sumLeft = nums
+            .slice(0, index)
+            .reduce((sum, leftNum) => (sum += leftNum), 0);
+        const sumRight = nums
+            .slice(index + 1)
+            .reduce((sum, leftNum) => (sum += leftNum), 0);
+        return sumLeft === sumRight;
+    });
+
+    return nums.indexOf(pivotNum);
+}
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,14 @@
-function defangIPaddr(nums) {}
+function defangIPaddr(nums) {
+    const newAddress = nums
+        .split(".")
+        .map((item, index) => {
+            if (index !== 0) {
+                return "[.]" + item;
+            } else {
+                return item;
+            }
+        })
+        .join("");
+
+    return newAddress;
+}
